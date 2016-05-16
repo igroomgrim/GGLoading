d429386ea9d50d9ab3a17beffc8d8e79170dd4c7
Anak Mirasing <thaihooligan@hotmail.com>
2016-05-16 12:56:57 +0700

    initial Xcode project and workspace

---
+++ GGLoading/GGLoading/GGLoading.h
@@ -0,0 +1,19 @@
+//
+//  GGLoading.h
+//  GGLoading
+//
+//  Created by Anak Mirasing on 5/16/16.
+//  Copyright © 2016 iGROOMGRiM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GGLoading.
+FOUNDATION_EXPORT double GGLoadingVersionNumber;
+
+//! Project version string for GGLoading.
+FOUNDATION_EXPORT const unsigned char GGLoadingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GGLoading/PublicHeader.h>
+
+
---
+++ GGLoading/GGLoadingTests/GGLoadingTests.swift
@@ -0,0 +1,36 @@
+//
+//  GGLoadingTests.swift
+//  GGLoadingTests
+//
+//  Created by Anak Mirasing on 5/16/16.
+//  Copyright © 2016 iGROOMGRiM. All rights reserved.
+//
+
+import XCTest
+@testable import GGLoading
+
+class GGLoadingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
